f18f85bbb4b75cf073529714fc704dedff9de760
saikiran <saikiran.davangams@gmail.com>
2022-02-06 00:21:05 +0100

    requirements satified for src

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "C-Users-KirAI-oops-Project-DVC-NLP-usecase"
+AUTHOR_USER_NAME = "saikiran"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
